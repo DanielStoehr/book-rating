a9fc9d115568fda9050e6775251fc412a2359300
Daniel Stoehr <daniel.stoehr@boschrexroth.de>
2023-01-23 15:43:54 +0100

    homework finished of day 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'br-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'Book Rating';
+  date = new Date();
+  timer: NodeJS.Timer;
+
+  constructor() {
+    this.timer = setInterval(() => {
+      this.date = new Date();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
 }
