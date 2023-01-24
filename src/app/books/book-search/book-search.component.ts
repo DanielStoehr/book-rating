7bd521fd972ecb6099200833b815c3510bd3cea4
Daniel Stoehr <daniel.stoehr@boschrexroth.de>
2023-01-24 10:07:02 +0100

    edit search component

--- src/app/books/book-search/book-search.component.ts
+++ src/app/books/book-search/book-search.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'br-book-search',
   templateUrl: './book-search.component.html',
-  styleUrls: ['./book-search.component.scss']
+  styleUrls: ['./book-search.component.scss'],
 })
 export class BookSearchComponent {
+  searchControl = new FormControl('', { nonNullable: true });
 
+  constructor() {
+    this.searchControl.valueChanges.subscribe((e) => console.log(e));
+  }
 }
